8c11db1399e261db03c62754038fbb14ddaef1f4
Tim Price <Tim.Price@resmed.com>
2018-11-08 14:06:52 +1100

    Added main application loop

--- src/marsrover/MarsRovers.java
+++ src/marsrover/MarsRovers.java
@@ -1,12 +1,40 @@
 package marsrover;
 
 public class MarsRovers {
-    Plateau m_plateau;
+    Plateau plateau;
+    InputParser ip;
     
 	MarsRovers() {
 	}
 	
-	void Run() {
-		m_plateau = new Plateau(1,1);
+	void run() {
+		try {
+			Class cls = this.getClass();
+			ip = new InputParser(cls.getResource("input.txt").getPath());
+			Point topRight = ip.parseUpperRightCoordinates();
+			plateau = new Plateau(topRight);
+			while (!ip.isEndOfFile()) {
+				Rover rover = new Rover(plateau, ip.parseRoverPosition());
+				char[] actions = ip.parseRoverActions();
+				for (char action : actions) {
+					switch(action) {
+						case 'L':
+							rover.turnLeft();
+							break;
+						case 'R':
+							rover.turnRight();
+							break;
+						case 'M':
+							rover.move();
+							break;
+						default:
+							throw new IllegalArgumentException();
+					}
+				}
+				System.out.print(rover.outputLocation());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
